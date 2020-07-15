c16e3ed4c74a552fa9810142f33f87cd97b467a6
Luis Guareschi <luisguareschi@hotmail.com>
2020-07-15 08:53:52 -0400

    small tweaks

--- Cambiar_Tamano_Imagen_UMarket/compress_resize.py
+++ Cambiar_Tamano_Imagen_UMarket/compress_resize.py
@@ -6,19 +6,21 @@ def compress_resize(file_path, compression_percentage=60):
     file_name = os.path.splitext(file_path)[0]
     image = Image.open(file_path)
     new_path = file_name + '_compressed.jpg'
-    image.save(new_path, optimize=True, quality=compression_percentage) # El porcentaje recomendado de compresion es 60, esta por default a ese numero
+    image.save(new_path, optimize=True, quality=compression_percentage)  # El porcentaje recomendado de compresion es 60, esta por default a ese numero
 
     # Abrir el archivo nuevo comprimido y crear dos imagenes nuevas, una en tamano mediano (200x200) y otra en tamano pequeno (40x50)
+    small_resolution = (200, 200)
+    medium_resolution = (40, 50)
     image = Image.open(new_path)
     file_name = os.path.splitext(new_path)[0]
-    medium_img = image.resize((200, 200), Image.ANTIALIAS)
-    small_img = image.resize((40, 50), Image.ANTIALIAS)
+    medium_img = image.resize(medium_resolution, Image.ANTIALIAS)
+    small_img = image.resize(small_resolution, Image.ANTIALIAS)
     medium_img.save(file_name+'_medium.jpg', optimize=True, quality=100)
     small_img.save(file_name+'_small.jpg', optimize=True, quality=100)
     small_path = file_name+'_small.jpg'
     medium_path = file_name+'_medium.jpg'
     big_path = new_path
-    print('Process done')
+    print('\tArchivo comprimido')
 
     # Se devuelven las nuevas direcciones de los archivos para que puedan ser usados facilmente
     return big_path, medium_path, small_path
@@ -30,12 +32,10 @@ def compress_resize(file_path, compression_percentage=60):
     '''
 
 ### Main Program ###
-all_folders = os.listdir('IMAGENES')
-for folder in all_folders:
-    f = 'IMAGENES\\'+folder
-    files = os.listdir(f)
+folder_path = 'IMAGENES' # Aqui se selecciona la carpeta madre de todas las imagenes que se deseen comprimir
+for subdir, dirs, files in os.walk(folder_path):
     for file in files:
-        path = f+'\\'+file
-        print('Editing file:', path, '...')
-        compress_resize(path)
-
+        file_path = subdir + os.sep + file
+        if file_path.endswith(".jpg") or file_path.endswith('.png'):
+            print('Comprimiendo:', file_path)
+            compress_resize(file_path)
